4807da9a005d28f4deef282207b0ce9596c75097
Hushraj Singh <56593086+saini128@users.noreply.github.com>
2025-05-11 16:45:52 +0530

    Create config.fish

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+function fish_greeting
+	fastfetch
+end
+function js
+      jarvis 
+      commandline -i (xclip -selection clipboard -o)
+end
